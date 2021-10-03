9ea0e87d9feac0d4f627484970ae1247d62d31c5
Niels Rogge <niels.rogge1@gmail.com>
2021-10-03 10:49:06 +0200

    Remove print statement

--- trocr/deit_models.py
+++ trocr/deit_models.py
@@ -542,8 +542,9 @@ class DeiTTRModel(FairseqEncoderDecoderModel):
         encoder_out = self.encoder(imgs, **kwargs)
 
         for k,v in encoder_out.items():
-            if len(v) > 0:
-                print(k,v[0].shape)
+            if k == "encoder_out":
+                print(f"Shape of {k}:",v[0].shape)
+                print(f"First elements of {k}:",v[:3,0,:3])
         
         decoder_out = self.decoder(
             prev_output_tokens, encoder_out=encoder_out, **kwargs
