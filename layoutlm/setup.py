b411ab3490220ad26cec4f8fe99b0339baee94a2
Yash Raj <120623663+unhingedpanda@users.noreply.github.com>
2024-03-27 22:58:20 +0530

    Update setup.py

--- layoutlm/setup.py
+++ layoutlm/setup.py
@@ -14,11 +14,11 @@ setup(
     packages=find_packages(exclude=("configs", "tests")),
     python_requires=">=3.6",
     install_requires=[
-        "transformers==4.27.2",  # Latest stable version
-        "tensorboardX==2.5.1",   # Latest stable version
-        "lxml==4.9.1",          # Latest stable version
-        "seqeval==1.2.1",        # Latest stable version
-        "Pillow==9.5.0",        # Latest stable version
+        "transformers",  # Latest stable version
+        "tensorboardX",   # Latest stable version
+        "lxml",          # Latest stable version
+        "seqeval",        # Latest stable version
+        "Pillow",        # Latest stable version
     ],
     extras_require={
         "dev": ["flake8==4.0.1", "isort==5.14.0", "black==22.3.0", "pre-commit==2.25.1"]
